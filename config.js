bb1a1264051b11328f4d43f9ea65612176770067
AmazbAlan <alymbekalmazbekov0@gmail.com>
2025-11-25 15:49:43 +0600

    start

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+// Вставь свои значения из Supabase
+export const SUPABASE_URL = "https://uutimydyolgmxssfcjsi.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1dGlteWR5b2xnbXhzc2ZjanNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNjIyNTUsImV4cCI6MjA3OTYzODI1NX0.d6MCg-OAJLMXZ8fLBjcmr-RSH8YK161EN8P7CrhPhdQ";
